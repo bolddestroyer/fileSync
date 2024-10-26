258d98bb30c9f78b1f1fa0af813bdb33b55b102a
bolddestroyer <banski.dorian@gmail.com>
2024-10-27 00:42:51 +0200

    V0.1

---
+++ main.py
@@ -0,0 +1,293 @@
+from tkinter import messagebox
+from modules import *
+from datetime import *
+from user_variables import *
+import os
+import shutil
+import re
+
+
+#Variables - messages for the log file
+log_txt_new_files = str()       #New files in USB
+log_txt_replaced_files = str()        #Replaced USB files
+log_txt_proc_res = str()        #Final result of the processing
+log_txt_nonex_insrc = str()      #USB files that do not exist in Dropbox or VSC
+log_txt_nonex_excldir = str()      #Non-existant directories listed as excluded
+
+#Variables - statistics
+stat_total_files = 0        #All processed files (non-excluded)
+stat_new_files = 0      #New files in USB
+stat_replaced_files = 0       #Replaced USB files
+stat_unchanged_files = 0      #Unchanged Dropbox and VSC files
+stat_nonex_insrc = 0      #USB files that do not exist in Dropbox or VSC
+
+
+##############################
+#Synchronization process for Dropbox, Virtual Studio Code files and USB
+def fileSynchronizer_db_usb_vsc_sync():
+
+    #Global variables
+    global log_txt_new_files, log_txt_replaced_files, log_txt_proc_res, log_txt_nonex_insrc, log_txt_nonex_excldir, stat_total_files, stat_new_files, stat_replaced_files, stat_unchanged_files, stat_nonex_insrc
+
+    print('Processing...')      #Message displayed in the console, indicating that processing has started
+
+    delete_jwlib_saves_from_usb(usb_dir, excl_usb_dirs)     #From 'fileSynchronizer_modules': delete all JW Library saves from USB
+
+    #Main 'try-catch' statement
+    try:
+        #Loop 1: iterate through all directories, subdirectories (list) and files (list) from Dropbox - synchronize between Dropbox and the USB
+        for dir_dp, sub_dirs_dp, files_dp in os.walk(dropbox_dir):
+            #Iterate through the list of files from the Dropbox directory
+            for file_dp in files_dp:
+
+                #Variables - directories
+                dropbox_file_l1 = os.path.join(dir_dp, file_dp)     #Path of the current Dropbox file
+                usb_dir_l1 = os.path.join(usb_dir, dir_dp[len(dropbox_dir):])       #USB directory corresponding to the current Dropbox directory
+                usb_file_l1 = os.path.join(usb_dir_l1, file_dp)     #Path of the USB file corresponding to the current Dropbox file
+
+                #If the Dropbox file is not excluded, or if the Dropbox directory is not excluded
+                if file_dp not in {'.dropbox', 'desktop.ini', 'marker_file'} and not excl_dropbox_dirs_regexp.search(dir_dp):
+                    #If the USB file corresponding to the current Dropbox file (same path) does not exist
+                    if not os.path.exists(usb_file_l1):
+                        #Create the Dropbox directory together with all it's parent directories in USB; exist_ok=True - do not return an error if a directory exists
+                        os.makedirs(usb_dir_l1, exist_ok=True)
+                        #Copy the file from Dropbox to USB
+                        shutil.copy(dropbox_file_l1, usb_dir_l1)
+                        log_txt_new_files += f"The file '{file_dp}' was created in '{usb_dir_l1}'\n"
+                        stat_total_files += 1
+                        stat_new_files += 1
+                    #If a Dropbox file exists in USB and if file last modified date in Dropbox is more recent (newer version of a file in Dropbox)
+                    elif os.path.exists(usb_file_l1) and (datetime.fromtimestamp(os.stat(dropbox_file_l1).st_mtime) > datetime.fromtimestamp(os.stat(usb_file_l1).st_mtime)):
+                        #Overwrite/replace a USB file by the same one from Dropbox
+                        shutil.copy(dropbox_file_l1, usb_dir_l1)
+                        log_txt_replaced_files += f"The file '{file_dp}' was replaced in '{usb_dir_l1}'\n"
+                        stat_total_files += 1
+                        stat_replaced_files += 1
+                    else:
+                        stat_total_files += 1
+                        stat_unchanged_files += 1
+            
+        #Loop 2: iterate through all directories, subdirectories (list) and files (list) from VSC - synchronize between VSC and the USB
+        for dir_md, sub_dirs_md, files_md in os.walk(vsc_dir):
+            #Iterate through the list of files from the VSC directory
+            for file_md in files_md:
+
+                #Variables - directories
+                vsc_file_l2 = os.path.join(dir_md, file_md)      #Path of the current VSC file
+                usb_dir_l2 = os.path.join(usb_dir, dir_md[len(vsc_dir):])       #USB directory corresponding to the current VSC directory
+                usb_file_l2 = os.path.join(usb_dir_l2, file_md)     #Path of the USB file corresponding to the current VSC file
+
+                #Variables - directories
+                vsc_regexp_excl_l2 = re.compile('.*.git.*|.*__pycache__.*|.*_exe.*|.*_log.*|.*chromedriver-win64.*')      #Regexp for excluded VSC directories
+                vsc_regexp_incl_l2 = re.compile('.*Virtual Studio Code.*')      #Regexp for the VSC directory
+
+                #If a VSC file does not exist in USB and if a VSC directory is not excluded
+                if not os.path.exists(usb_file_l2) and vsc_regexp_incl_l2.search(dir_md) and not vsc_regexp_excl_l2.search(dir_md):
+                    #Create the VSC directory together with all it's parent directories in USB; exist_ok=True - do not return an error if a directory exists
+                    os.makedirs(usb_dir_l2, exist_ok=True)
+                    #Copy the file from VSC to USB
+                    shutil.copy(vsc_file_l2, usb_dir_l2)
+                    log_txt_new_files += f"The file '{file_md}' was created in '{usb_dir_l2}'\n"
+                    stat_total_files += 1
+                    stat_new_files += 1
+                #If a VSC file does not exist in USB, if a VSC directory is not excluded, and if file last modified date in VSC is more recent (newer version of a file in VSC)
+                elif os.path.exists(usb_file_l2) and vsc_regexp_incl_l2.search(dir_md) and not vsc_regexp_excl_l2.search(dir_md) and (datetime.fromtimestamp(os.stat(vsc_file_l2).st_mtime) > datetime.fromtimestamp(os.stat(usb_file_l2).st_mtime)):
+                    #Overwrite/replace a USB file by the same one from VSC
+                    shutil.copy(vsc_file_l2, usb_dir_l2)
+                    log_txt_replaced_files += f"The file '{file_md}' was replaced in '{usb_dir_l2}'\n"
+                    stat_total_files += 1
+                    stat_replaced_files += 1
+                else:
+                    stat_total_files += 1
+                    stat_unchanged_files += 1
+
+        #Loop 3: iterate through all directories, subdirectories (list) and files (list) from USB - check if files from USB exist in Dropbox and VSC, apart for those excluded
+        for dir_usb, sub_dirs_usb, files_usb in os.walk(usb_dir):
+            #Iterate through the list of files from the USB directory
+            for file_usb in files_usb:
+
+                #Variables - directories
+                drobpox_dir_l3 = os.path.join(dropbox_dir, dir_usb[len(usb_dir):])      #Dropbox directory corresponding to the current USB directory
+                vsc_dir_l3 = os.path.join(vsc_dir, dir_usb[len(usb_dir):])      #VSC directory corresponding to the current USB directory
+                dropbox_file_l3 = os.path.join(dropbox_dir, dir_usb[len(usb_dir):], file_usb)       #Dropbox file corresponding to the current USB file
+                vsc_file_l3 = os.path.join(vsc_dir, dir_usb[len(usb_dir):], file_usb)       #VSC file corresponding to the current USB file
+
+                #Variables - excluded directories
+                vsc_regexp_excl_l3 = re.compile('.*Virtual Studio Code.*')      #Regexp for excluded VSC directories
+
+                #If a USB directory is not excluded
+                if dir_usb not in excl_usb_dirs and not excl_usb_dirs_regexp.search(dir_usb):
+                    #If a USB file does not exist in Dropbox, if it is not a VSC directory and if a USB file is not excluded
+                    if not os.path.exists(dropbox_file_l3) and not vsc_regexp_excl_l3.search(dir_usb) and file_usb not in {'INVITATION GERMAN.pdf', 'Notfallliste Predigtdienstgruppen ab dem 22.01.2024.xlsx'}:
+                        log_txt_nonex_insrc += f'The file "{file_usb}" does not exist in "{drobpox_dir_l3}".\n'
+                        stat_nonex_insrc += 1
+                    #If a USB file does not exist in VSC and if it is a VSC directory
+                    elif not os.path.exists(vsc_file_l3) and vsc_regexp_excl_l3.search(dir_usb):
+                        log_txt_nonex_insrc += f'The file "{file_usb}" does not exist in "{vsc_dir_l3}".\n'
+                        stat_nonex_insrc += 1
+
+        #Iterate through a list of excluded USB directories
+        for excl_usb_dir in excl_usb_dirs:
+            #If an excluded USB directory does not exist
+            if not os.path.exists(excl_usb_dir):
+                #Add a string to the log text, provided that it does not already exist in the log text
+                log_txt_nonex_excldir = add_if_logtxt_noex(log_txt_nonex_excldir, f'USB directory "{excl_usb_dir}" listed as excluded, does not exist.')
+
+    #Exceptions - in case of OSErrors (input directory not found) and PermissionError (insufficient permissions to perform requested action)
+    except (OSError, PermissionError) as e:
+        log_txt_proc_res = f"ERROR: {str(e)}.\n"
+        messagebox.showerror("fileSynchronizer: Error", f"{str(e)}")
+        sys.exit()
+    else:
+        log_txt_proc_res = "File synchronization successful!"      #If processing is successful
+    
+    #Print out the processing result in the console
+    print(log_txt_proc_res)
+
+    #Create a log file
+    log_file(dropbox_dir, vsc_dir, usb_dir, log_txt_proc_res, stat_total_files, stat_unchanged_files, stat_new_files, log_txt_new_files, stat_replaced_files, log_txt_replaced_files, log_txt_nonex_excldir, stat_nonex_insrc, log_txt_nonex_insrc, log_file_dir, log_file_rundate)
+
+    #Display the messagebox
+    custom_messagebox(log_file_dir, log_txt_proc_res, log_file_rundate)
+
+
+##############################
+#Analysis of Dropbox, Virtual Studio Code and USB files, with no synchronization
+def fileSynchronizer_db_usb_vsc_anal():
+
+    #Global variables
+    global log_txt_new_files, log_txt_replaced_files, log_txt_proc_res, log_txt_nonex_insrc, log_txt_nonex_excldir, stat_total_files, stat_new_files, stat_replaced_files, stat_unchanged_files, stat_nonex_insrc
+
+    print('Processing...')      #Message displayed in the console, indicating that processing has started
+
+    #Main 'try-catch' statement
+    try:
+        #Loop 1: iterate through all directories, subdirectories (list) and files (list) from Dropbox - synchronize between Dropbox and the USB
+        for dir_dp, sub_dirs_dp, files_dp in os.walk(dropbox_dir):
+            #Iterate through the list of files from the Dropbox directory
+            for file_dp in files_dp:
+
+                #Variables - directories
+                dropbox_file_l1 = os.path.join(dir_dp, file_dp)     #Path of the current Dropbox file
+                usb_dir_l1 = os.path.join(usb_dir, dir_dp[len(dropbox_dir):])       #USB directory corresponding to the current Dropbox directory
+                usb_file_l1 = os.path.join(usb_dir_l1, file_dp)     #Path of the USB file corresponding to the current Dropbox file
+
+                #If the Dropbox file is not excluded, or if the Dropbox directory is not excluded
+                if file_dp not in {'.dropbox', 'desktop.ini', 'marker_file'} and not excl_dropbox_dirs_regexp.search(dir_dp):
+                    #If the USB file corresponding to the current Dropbox file (same path) does not exist
+                    if not os.path.exists(usb_file_l1):
+                        log_txt_new_files += f"The file '{file_dp}' does not exist in USB.\n"
+                        stat_total_files += 1
+                        stat_new_files += 1
+                    #If a Dropbox file exists in USB and if file last modified date in Dropbox is more recent (newer version of a file in Dropbox)
+                    elif os.path.exists(usb_file_l1) and (datetime.fromtimestamp(os.stat(dropbox_file_l1).st_mtime) > datetime.fromtimestamp(os.stat(usb_file_l1).st_mtime)):
+                        log_txt_replaced_files += f"New version of '{file_dp}' from the USB directory '{usb_dir_l1}' exists in Dropbox.\n"
+                        stat_total_files += 1
+                        stat_replaced_files += 1
+                    else:
+                        stat_total_files += 1
+                        stat_unchanged_files += 1
+            
+        #Loop 2: iterate through all directories, subdirectories (list) and files (list) from VSC - synchronize between VSC and the USB
+        for dir_md, sub_dirs_md, files_md in os.walk(vsc_dir):
+            #Iterate through the list of files from the VSC directory
+            for file_md in files_md:
+
+                #Variables - directories
+                vsc_file_l2 = os.path.join(dir_md, file_md)      #Path of the current VSC file
+                usb_dir_l2 = os.path.join(usb_dir, dir_md[len(vsc_dir):])       #USB directory corresponding to the current VSC directory
+                usb_file_l2 = os.path.join(usb_dir_l2, file_md)     #Path of the USB file corresponding to the current VSC file
+
+                #Variables - directories
+                vsc_regexp_excl_l2 = re.compile('.*.git.*|.*__pycache__.*|.*_exe.*|.*_log.*|.*chromedriver-win64.*')      #Regexp for excluded VSC directories
+                vsc_regexp_incl_l2 = re.compile('.*Virtual Studio Code.*')      #Regexp for the VSC directory
+
+                #If a VSC file does not exist in USB and if a VSC directory is not excluded
+                if not os.path.exists(usb_file_l2) and vsc_regexp_incl_l2.search(dir_md) and not vsc_regexp_excl_l2.search(dir_md):
+                    log_txt_new_files += f"The file '{file_md}' does not exist in USB.\n"
+                    stat_total_files += 1
+                    stat_new_files += 1
+                #If a VSC file does not exist in USB, if a VSC directory is not excluded, and if file last modified date in VSC is more recent (newer version of a file in VSC)
+                elif os.path.exists(usb_file_l2) and vsc_regexp_incl_l2.search(dir_md) and not vsc_regexp_excl_l2.search(dir_md) and (datetime.fromtimestamp(os.stat(vsc_file_l2).st_mtime) > datetime.fromtimestamp(os.stat(usb_file_l2).st_mtime)):
+                    log_txt_replaced_files += f"New version of '{file_md}' from the USB directory '{usb_dir_l2}' exists in Dropbox.'\n"
+                    stat_total_files += 1
+                    stat_replaced_files += 1
+                else:
+                    stat_total_files += 1
+                    stat_unchanged_files += 1
+
+        #Loop 3: iterate through all directories, subdirectories (list) and files (list) from USB - check if files from USB exist in Dropbox and VSC, apart for those excluded
+        for dir_usb, sub_dirs_usb, files_usb in os.walk(usb_dir):
+            #Iterate through the list of files from the USB directory
+            for file_usb in files_usb:
+
+                #Variables - directories
+                drobpox_dir_l3 = os.path.join(dropbox_dir, dir_usb[len(usb_dir):])      #Dropbox directory corresponding to the current USB directory
+                vsc_dir_l3 = os.path.join(vsc_dir, dir_usb[len(usb_dir):])      #VSC directory corresponding to the current USB directory
+                dropbox_file_l3 = os.path.join(dropbox_dir, dir_usb[len(usb_dir):], file_usb)       #Dropbox file corresponding to the current USB file
+                vsc_file_l3 = os.path.join(vsc_dir, dir_usb[len(usb_dir):], file_usb)       #VSC file corresponding to the current USB file
+
+                #Variables - excluded directories
+                vsc_regexp_excl_l3 = re.compile('.*Virtual Studio Code.*')      #Regexp for excluded VSC directories
+
+                #If a USB directory is not excluded
+                if dir_usb not in excl_usb_dirs and not excl_usb_dirs_regexp.search(dir_usb):
+                    #If a USB file does not exist in Dropbox, if it is not a VSC directory and if a USB file is not excluded
+                    if not os.path.exists(dropbox_file_l3) and not vsc_regexp_excl_l3.search(dir_usb) and file_usb not in {'INVITATION GERMAN.pdf', 'Notfallliste Predigtdienstgruppen ab dem 22.01.2024.xlsx'}:
+                        log_txt_nonex_insrc += f'The file "{file_usb}" does not exist in "{drobpox_dir_l3}".\n'
+                        stat_nonex_insrc += 1
+                    #If a USB file does not exist in VSC and if it is a VSC directory
+                    elif not os.path.exists(vsc_file_l3) and vsc_regexp_excl_l3.search(dir_usb):
+                        log_txt_nonex_insrc += f'The file "{file_usb}" does not exist in "{vsc_dir_l3}".\n'
+                        stat_nonex_insrc += 1
+
+        #Iterate through a list of excluded USB directories
+        for excl_usb_dir in excl_usb_dirs:
+            #If an excluded USB directory does not exist
+            if not os.path.exists(excl_usb_dir):
+                #Add a string to the log text, provided that it does not already exist in the log text
+                log_txt_nonex_excldir = add_if_logtxt_noex(log_txt_nonex_excldir, f'USB directory "{excl_usb_dir}" listed as excluded, does not exist.')
+
+    #Exceptions - in case of OSErrors (input directory not found) and PermissionError (insufficient permissions to perform requested action)
+    except (OSError, PermissionError) as e:
+        log_txt_proc_res = f"ERROR: {str(e)}.\n"
+        messagebox.showerror("fileSynchronizer: Error", f"{str(e)}")
+        print(log_txt_proc_res)
+        sys.exit()
+    else:
+        log_txt_proc_res = "Analysis successful!"      #If processing is successful
+    
+    #Print out the processing result in the console
+    print(log_txt_proc_res)
+
+    #Create a log file
+    log_file(dropbox_dir, vsc_dir, usb_dir, log_txt_proc_res, stat_total_files, stat_unchanged_files, stat_new_files, log_txt_new_files, stat_replaced_files, log_txt_replaced_files, log_txt_nonex_excldir, stat_nonex_insrc, log_txt_nonex_insrc, log_file_dir, log_file_rundate)
+
+    #Display the messagebox
+    custom_messagebox(log_file_dir, log_txt_proc_res, log_file_rundate)
+
+
+##############################
+#Main window, allows selecting between sychronization and analysis
+def fileSynchronizer_window():
+    window = tkinter.Tk()
+    window.title("FileSynchronizer")
+
+    lbl_main = tkinter.Label(window, text="Select an option")
+    lbl_main.grid(row=0, column=0, padx=10, pady=10)
+
+    btn_db_usb_vsc_sync = tkinter.Button(window, text="Run sychronization between Dropbox, VSC and USB", command=lambda: fileSynchronizer_db_usb_vsc_sync())
+    btn_db_usb_vsc_sync.grid(row=1, column=0, padx=10, pady=10)
+
+    btn_db_usb_vsc_anal = tkinter.Button(window, text="Run analysis between Dropbox, VSC and USB", command=lambda: fileSynchronizer_db_usb_vsc_anal())
+    btn_db_usb_vsc_anal.grid(row=2, column=0, padx=10, pady=10)
+
+    #Display the message box in the middle of the screen
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    x = (window.winfo_screenwidth() // 2) - (width // 2)
+    y = (window.winfo_screenheight() // 2) - (height // 2)
+    window.geometry(f'{width}x{height}+{x}+{y}')
+
+    window.mainloop()
---
+++ modules.py
@@ -0,0 +1,118 @@
+#File with modules for the fileSynchronizer_main
+import csv
+import os
+import re
+from datetime import *
+import subprocess
+import sys
+import tkinter
+
+
+#Read excluded directories out of a CSV-file and pack them into a list
+def read_csv_excl_dirs(csv_file_dir):
+    
+    #Variables - list of excluded directories from the CSV-file
+    excl_dirs = []
+
+    #Read the CSV-file
+    with open(csv_file_dir, newline='', encoding='utf-8') as csv_file:
+        file_excl_dirs = csv.reader(csv_file)       #Read all entries from the file, create a reader object
+        next(file_excl_dirs, None)      #Skip the 1st line of the CSV-file
+        #Iterate through all entries of the entries from the CSV-file
+        for row in file_excl_dirs:
+            excl_dirs.append(row[0])
+   
+    #Return the list of excluded directories
+    return excl_dirs
+
+
+#If a string does not already exist in the log_text, add it to the log_text
+def add_if_logtxt_noex(log_text, string_val):
+    
+    #If the new log_text is not already present in the log_text string, add it
+    if string_val not in log_text:
+        log_text += f'{string_val}\n'
+    
+    return log_text
+
+
+#Delete all JW Library saves from USB
+def delete_jwlib_saves_from_usb(usb_dir, excl_usb_dirs):
+    
+    #Variables - regexp for jwlibrary saves
+    jwlib_save_regexp = re.compile('UserdataBackup.*.jwlibrary')
+
+    #Loop: iterate through all directories, subdirectories (list) and files (list) from USB
+    for dir_usb, sub_dirs_usb, files_usb in os.walk(usb_dir):
+        #Iterate through the list of files from the USB directory
+        for file_usb in files_usb:
+            #If a USB file is a JWLibrary save and is not excluded
+            if jwlib_save_regexp.search(file_usb) and dir_usb not in excl_usb_dirs:
+                os.remove(os.path.join(dir_usb, file_usb))
+
+
+#Log file
+def log_file(dropbox_dir, vsc_dir, usb_dir, log_txt_proc_res, stat_total_files, stat_unchanged_files, stat_new_files, log_txt_new_files, stat_replaced_files, log_txt_replaced_files, log_txt_nonex_excldir, stat_nonex_insrc, log_txt_nonex_insrc, log_file_dir, log_file_rundate):
+    #Composition of the log file
+    with open(log_file_dir, 'w', encoding='utf-8') as log_file:
+        log_file.write('\n-----------------------------THE FILE SYNCHRONIZER------------------------------\n\n')
+        log_file.write(f'Dropbox directory: {dropbox_dir}\n')
+        log_file.write(f'VSC directory: {vsc_dir}\n')
+        log_file.write(f'USB directory: {usb_dir}\n')
+        log_file.write('\n--------------------------------------------------------------------------------\n')
+        log_file.write(f'Synchronization run on {log_file_rundate}\n')
+        log_file.write(f'Result of the synchronization: {log_txt_proc_res}\n')
+        log_file.write(f'\n-----------------------------------STATISTICS-----------------------------------\n')
+        log_file.write(f'Total number of files: {stat_total_files}\n')
+        log_file.write(f'Unchanged files: {stat_unchanged_files}\n\n')
+        log_file.write(f'New files: {stat_new_files}\n')
+        log_file.write(log_txt_new_files)
+        log_file.write(f'\nReplaced files: {stat_replaced_files}\n')
+        log_file.write(log_txt_replaced_files)
+        
+        log_file.write('\n------------------------OTHER SOURCE DESTINATION ANALYSIS------------------------\n')
+
+        if log_txt_nonex_excldir != "":
+            log_file.write(f'Non-existing excluded directories:\n{log_txt_nonex_excldir}')
+        else:
+            log_file.write(f'Non-existing excluded directories: None\n')
+
+        if stat_nonex_insrc != 0:
+            log_file.write(f'\nFiles missing in Dropbox and VSC: {stat_nonex_insrc}\n')
+            log_file.write(log_txt_nonex_insrc)
+        else:
+            log_file.write(f'\nFiles missing in Dropbox and VSC: None')
+
+
+def open_log_file(log_file_dir):
+    subprocess.Popen([r"C:\Program Files\Notepad++\notepad++.exe", log_file_dir], shell=True)
+
+
+#Custom message box with buttons to close and open the log file
+def custom_messagebox(log_file_dir, log_txt_proc_res, log_file_rundate):
+    #Top-level pop-up window
+    msg_box = tkinter.Tk()
+    msg_box.title('fileSynchronizer processing info')
+    
+    #Labels
+    lbl_status = tkinter.Label(msg_box, text=log_txt_proc_res)
+    lbl_status.grid(row=0, column=0, columnspan=2)
+    lbl_runtime = tkinter.Label(msg_box, text=f'Synchronization run on {log_file_rundate}')
+    lbl_runtime.grid(row=1, column=0, columnspan=2)
+
+    #Button - close the message box and end the process
+    btn_ok = tkinter.Button(msg_box, text="OK", command=lambda: sys.exit())
+    btn_ok.grid(row=2, column=0, padx=10, pady=10)
+    
+    #Button - open the log file
+    #Open the log file (subprocess - interact with OS and launch external process; Popen - create a new process; shell=True - run through cmd)
+    btn_open_log = tkinter.Button(msg_box, text="Open the log file", command=lambda: [open_log_file(log_file_dir), sys.exit()])
+    btn_open_log.grid(row=2, column=1, padx=10, pady=10)
+
+    #Display the message box in the middle of the screen
+    msg_box.update_idletasks()
+    width = msg_box.winfo_width()
+    height = msg_box.winfo_height()
+    x = (msg_box.winfo_screenwidth() // 2) - (width // 2)
+    y = (msg_box.winfo_screenheight() // 2) - (height // 2)
+    msg_box.geometry(f'{width}x{height}+{x}+{y}')
