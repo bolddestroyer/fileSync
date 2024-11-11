0f3b7f7036010b8532c74a0b3c9832152e9a0bce
bolddestroyer <banski.dorian@gmail.com>
2024-11-11 02:05:41 +0100

    V2.1

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from datetime import *
 from fileSync_variables import *
 import os
 import shutil
+import time
 
 
-def fileSync():
+def fileSync(update_progress_callback=None):
 
     log_lbl_newFiles = str()       #New files in target
     log_lbl_replacedFiles = str()        #Replaced files in target
@@ -19,9 +20,17 @@ def fileSync():
     stat_unchangedFiles = 0      #Unchanged files in source
     stat_nonExistInSrc = 0      #Files from target that do not exist in source
 
+    start_time = time.time()    #Record the time when the execution was started
+
     remove_jwlib_saves_from_usb(dir_trg)     #Nodules: delete all JW Library saves from target
 
     try:
+        #Count all files in the source directory - required for progress calculation
+        total_files = sum(1 for dir_src_dir, dir_src_subdir, dir_src_files in os.walk(dir_src)
+                            for dir_src_file in dir_src_files
+                            if dir_src_file not in excl_dir_src_file and not excl_src_regexp.search(dir_src_dir))
+        processed_files = 0
+
         #Iterate through all directories, subdirectories (list) and files (list) from the source
         for dir_src_dir, dir_src_subdir, dir_src_files in os.walk(dir_src):
             for dir_src_file in dir_src_files:
@@ -38,6 +47,7 @@ def fileSync():
                         #Copy the file from source to target
                         shutil.copy(src_file, trg_curr_dir)
                         log_lbl_newFiles += f"The file '{dir_src_file}' was created in '{trg_curr_dir}'\n"
+                        processed_files += 1
                         stat_totalFiles += 1
                         stat_newFiles += 1
                     #If source file exists in target and if file last modified date in source is more recent (newer version of a file in source)
@@ -45,11 +55,16 @@ def fileSync():
                         #Overwrite/replace a target file by the same one from source
                         shutil.copy(src_file, trg_curr_dir)
                         log_lbl_replacedFiles += f"The file '{dir_src_file}' was replaced in '{trg_curr_dir}'\n"
+                        processed_files += 1
                         stat_totalFiles += 1
                         stat_replacedFiles += 1
                     else:
+                        processed_files += 1
                         stat_totalFiles += 1
                         stat_unchangedFiles += 1
+                
+                if update_progress_callback:
+                    update_progress_callback(processed_files, total_files)
 
         #Iterate through all directories, subdirectories (list) and files (list) from the target
         for dir_trg_dir, dir_trg_subdir, dir_trg_files in os.walk(dir_trg):
@@ -61,7 +76,7 @@ def fileSync():
                 #If target directory is not excluded
                 if not excl_trg_regexp.search(dir_trg_dir):
                     #If target file does not exist in corresponding source directory
-                    if not os.path.exists(src_file) and dir_trg_file not in {'INVITATION GERMAN.pdf', 'Notfallliste Predigtdienstgruppen ab dem 22.01.2024.xlsx'}:
+                    if not os.path.exists(src_file) and dir_trg_file not in excl_trg_files:
                         log_lbl_nonExistInSrc += f'The file "{dir_trg_file}" does not exist in "{src_curr_dir}".\n'
                         stat_nonExistInSrc += 1
 
@@ -71,25 +86,31 @@ def fileSync():
         messagebox.showerror("fileSynchronizer: Error", f"{str(e)}")
         sys.exit()
     else:
+        processing_duration = time.time() - start_time 
         log_lbl_processResult = "File synchronization successful!"
-    
-    #Print out the processing result in the console
-    print(log_lbl_processResult)
 
     #Create a log file
-    log_file(dir_src, dir_trg, log_lbl_processResult, stat_totalFiles, stat_unchangedFiles, stat_newFiles, log_lbl_newFiles, stat_replacedFiles, log_lbl_replacedFiles, stat_nonExistInSrc, log_lbl_nonExistInSrc, log_dir, log_rundate)
+    log_file(dir_src, dir_trg, log_lbl_processResult, stat_totalFiles, stat_unchangedFiles, stat_newFiles, log_lbl_newFiles, stat_replacedFiles, log_lbl_replacedFiles, stat_nonExistInSrc, log_lbl_nonExistInSrc, log_dir, log_rundate, processed_files, total_files, processing_duration)
 
     #Display the messagebox
-    custom_messagebox(log_dir, log_lbl_processResult, log_rundate)
+    custom_messagebox(log_dir, log_lbl_processResult, log_rundate, processed_files, total_files, processing_duration)
 
 
 #Main window
 def fileSync_window():
     window = tkinter.Tk()
-    window.title("FileSync")
+    window.title("fileSync")
 
-    btn_db_usb_vsc_sync = tkinter.Button(window, text="Run sychronization", command=lambda: fileSync())
-    btn_db_usb_vsc_sync.grid(row=1, column=0, padx=10, pady=10)
+    window_lbl = tkinter.Label(window, text="Click the below button to start the synchronization")
+    window_lbl.grid(row=0, column=0, padx=1, pady=1, sticky="nsew")
+
+    window_btn = tkinter.Button(window, text="Run sychronization", command=lambda: fileSync())
+    window_btn.grid(row=1, column=0, pady=10)
+
+    def update_button_text(processed_files, total_files):
+        window_btn.config(text=f"{processed_files} of {total_files} files processed")
+
+    window_btn.config(command=lambda: fileSync(update_progress_callback=update_button_text))
 
     #Display the message box in the middle of the screen
     window.update_idletasks()
@@ -100,3 +121,5 @@ def fileSync_window():
     window.geometry(f'{width}x{height}+{x}+{y}')
 
     window.mainloop()
+
+
--- modules.py
+++ modules.py
@@ -24,18 +24,19 @@ def remove_jwlib_saves_from_usb(dir_trg):
         for dir_trg_file in dir_trg_files:
             #Remove if files file is a JWLibrary save
             if jwlibsave_regexp.search(dir_trg_file):
-                print(os.path.join(dir_trg, dir_trg_dirs, dir_trg_file))
                 os.remove(os.path.join(dir_trg, dir_trg_dirs, dir_trg_file))
 
 
 #Log file
-def log_file(dir_src, dir_trg, log_lbl_processResult, stat_totalFiles, stat_unchangedFiles, stat_newFiles, log_lbl_newFiles, stat_replacedFiles, log_lbl_replacedFiles, stat_nonExistInSrc, log_lbl_nonExistInSrc, log_dir, log_rundate):
+def log_file(dir_src, dir_trg, log_lbl_processResult, stat_totalFiles, stat_unchangedFiles, stat_newFiles, log_lbl_newFiles, stat_replacedFiles, log_lbl_replacedFiles, stat_nonExistInSrc, log_lbl_nonExistInSrc, log_dir, log_rundate, processed_files, total_files, processing_duration):
 
     with open(log_dir, 'w', encoding='utf-8') as log_file:
         log_file.write('-----------------------------THE FILE SYNCHRONIZER------------------------------\n')
         log_file.write(f'Source directory: {dir_src}\n')
         log_file.write(f'Target directory: {dir_trg}\n')
         log_file.write(f'Synchronization run on {log_rundate}\n')
+        log_file.write(f'Processed files: {processed_files}/{total_files}\n')
+        log_file.write(f'Processing duration: {processing_duration:.2f} seconds\n')
         log_file.write(f'Result: {log_lbl_processResult}\n')
         log_file.write(f'\n-----------------------------------STATISTICS-----------------------------------\n')
         log_file.write(f'Total number of files: {stat_totalFiles}\n')
@@ -57,7 +58,7 @@ def open_log_file(log_dir):
 
 
 #Custom message box with buttons to close and open the log file
-def custom_messagebox(log_dir, log_lbl_processResult, log_rundate):
+def custom_messagebox(log_dir, log_lbl_processResult, log_rundate, processed_files, total_files, processing_duration):
     #Top-level pop-up window
     msg_box = tkinter.Tk()
     msg_box.title('fileSynchronizer processing info')
@@ -65,17 +66,24 @@ def custom_messagebox(log_dir, log_lbl_processResult, log_rundate):
     #Labels
     lbl_status = tkinter.Label(msg_box, text=log_lbl_processResult)
     lbl_status.grid(row=0, column=0, columnspan=2)
+    
+    lbl_proc_files = tkinter.Label(msg_box, text=f'Processed files: {processed_files}/{total_files}')
+    lbl_proc_files.grid(row=1, column=0, columnspan=2)
+
+    lbl_proc_duration = tkinter.Label(msg_box, text=f'Processing duration: {processing_duration:.2f} seconds')
+    lbl_proc_duration.grid(row=2, column=0, columnspan=2)
+
     lbl_runtime = tkinter.Label(msg_box, text=f'Synchronization run on {log_rundate}')
-    lbl_runtime.grid(row=1, column=0, columnspan=2)
+    lbl_runtime.grid(row=3, column=0, columnspan=2)
 
     #Button - close the message box and end the process
     btn_ok = tkinter.Button(msg_box, text="OK", command=lambda: sys.exit())
-    btn_ok.grid(row=2, column=0, padx=10, pady=10)
+    btn_ok.grid(row=4, column=0, padx=10, pady=10)
     
     #Button - open the log file
     #Open the log file (subprocess - interact with OS and launch external process; Popen - create a new process; shell=True - run through cmd)
     btn_open_log = tkinter.Button(msg_box, text="Open the log file", command=lambda: [open_log_file(log_dir), sys.exit()])
-    btn_open_log.grid(row=2, column=1, padx=10, pady=10)
+    btn_open_log.grid(row=4, column=1, padx=10, pady=10)
 
     #Display the message box in the middle of the screen
     msg_box.update_idletasks()
